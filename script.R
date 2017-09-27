30bf9c93e61d48a0fe20a0bc7b34dabeefa94529
yszhangit <yinshu.zhang.git@gmail.com>
2017-09-27 15:13:03 -0400

    day3

--- script.R
+++ script.R
@@ -1,18 +1,20 @@
 library(dplyr)
+library(magrittr)
+library(knitr)
 
-envcat <- read.csv("envcat.txt")
-eventcat<-sort(toupper((envcat[,1])))
+evtcat <- read.csv("evtcat.txt")
+eventcat<-sort(toupper((evtcat[,1])))
+rm(evtcat)
 
-dat<-read.csv("repdata%2Fdata%2FStormData.csv", stringsAsFactors = F)
+dat<-read.csv("../repdata%2Fdata%2FStormData.csv", stringsAsFactors = F)
 dat<-tbl_df(dat)
 dat<-dat %>% mutate(EVTTYPE = toupper(trimws(EVTYPE)))
 
-dat1<-dat %>% mutate(PROPDMGEXP = toupper(trimws(PROPDMGEXP)),
+dat<-dat %>% mutate(PROPDMGEXP = toupper(trimws(PROPDMGEXP)),
                     CROPDMGEXP = toupper(trimws(CROPDMGEXP)),
-                    EVTYPE = toupper(trimws(EVTYPE)))
-
-dat1<-mutate(dat1, event_cat = NA)
-
+                    EVTYPE = toupper(trimws(EVTYPE)),
+                    event_cat = NA
+                    )
 
 assign_event_cat <- function(ev,ec) {
   for ( i in 1:length(ev)) {
@@ -20,24 +22,64 @@ assign_event_cat <- function(ev,ec) {
     dat1<<-dat1
   }
 }
+event_types <- c(
+  "TSTM",
+  "EXTREME COLD",
+  "EXTREME HEAT",
+  "WILD/FOREST FIRE",
+  "URBAN/SML STREAM FLD",
+  "SNOW",
+  "FOG",
+  "URBAN FLOOD",
+  "WIND",
+  "EXTREME WINDCHILL",
+  "FREEZING RAIN",
+  "HEAVY SURF/HIGH SURF",
+  "HURRICANE",
+  "RIVER FLOOD",
+  "STORM SURGE",
+  "ASTRONOMICAL LOW TIDE",
+  "COLD"
+)
 
+event_cats <- c(
+  "THUNDERSTORM",
+  "EXTREME COLD/WIND CHILL",
+  "HEAT",
+  "WILDFIRE",
+  "FLOOD",
+  "HEAVY SNOW",
+  "DENSE FOG",
+  "FLOOD",
+  "HIGH WIND",
+  "EXTREME COLD/WIND CHILL",
+  "SLEET",
+  "HIGH SURF",
+  "HURRICANE (TYPHOON)",
+  "FLOOD",
+  "STORM SURGE/TIDE",
+  "ASTRONOMICAL LOW TIDE",
+  "COLD/WIND CHILL"
+)
+assign_event_cat(event_types, event_cats)
 
 for ( e in eventcat) {
   dat1[startsWith(dat1$EVTYPE , e), "event_cat"] <- e
 }
 
+
 dat1$propval <- expval(dat1$PROPDMG, dat1$PROPDMGEXP)
 dat1$cropval <- expval(dat1$CROPDMG, dat1$CROPDMGEXP)
 
 # check 
-dat1_sum<-dat1 %>% 
+dat1 %>% 
   filter(is.na(event_cat))  %>% 
   group_by(EVTYPE) %>% 
   summarize( 
     cnt = n(), 
     total_fat = sum(FATALITIES), 
     total_inj = sum(INJURIES),
-    total_propval =  sum(propval)
+    total_propval =  round(sum(propval)/1000000,2)
   ) %>% 
   arrange(desc(cnt))
 
--- script_sample.R
+++ script_sample.R
@@ -35,7 +35,7 @@ expval_dont<-function(v,e) {
 # but this is a fake vector function, i still got same warning message
 # Grouping rowwise data frame strips rowwise nature 
 
-Vexpval<-Vectorize(expval, vectorize.args = c("v","e"))
+# Vexpval<-Vectorize(expval, vectorize.args = c("v","e"))
 
 # apply function to each row, because function take row, mutate will pass list to function call, not individual element
 #
@@ -84,44 +84,67 @@ sam$cropval <- expval(sam$CROPDMG, sam$CROPDMGEXP)
 # use unlist or pull to extract a column from data frame if you forgot again how to do it
 
 
-sam1<-mutate(sam, event_cat = NA)
+sam<-mutate(sam, event_cat = NA)
 
 # clean up event type
 
 assign_event_cat_sample <- function(ev,ec) {
   for ( i in 1:length(ev)) {
-    sam1[grep(ev[i],sam1$EVTTYPE),] %<>% mutate(event_cat = gsub(ev[i],ec[i], EVTYPE))
-    sam1<<-sam1
+    sam[grep(ev[i],sam$EVTYPE),] %<>% mutate(event_cat = gsub(ev[i],ec[i], EVTYPE))
+    sam<<-sam
   }
 }
-
-event_types=c(
+# event_types and event_cats has to match as pairs
+event_types <- c(
   "TSTM",
   "EXTREME COLD",
   "EXTREME HEAT",
   "WILD/FOREST FIRE",
-  "URBAN/SML STREAM FLD"
+  "URBAN/SML STREAM FLD",
+  "SNOW",
+  "FOG",
+  "URBAN FLOOD",
+  "WIND",
+  "EXTREME WINDCHILL",
+  "FREEZING RAIN",
+  "HEAVY SURF/HIGH SURF",
+  "HURRICANE",
+  "RIVER FLOOD",
+  "STORM SURGE",
+  "ASTRONOMICAL LOW TIDE",
+  "COLD"
 )
 
-event_cats=c(
+event_cats <- c(
   "THUNDERSTORM",
   "EXTREME COLD/WIND CHILL",
   "HEAT",
   "WILDFIRE",
-  "FLOOD"
+  "FLOOD",
+  "HEAVY SNOW",
+  "DENSE FOG",
+  "FLOOD",
+  "HIGH WIND",
+  "EXTREME COLD/WIND CHILL",
+  "SLEET",
+  "HIGH SURF",
+  "HURRICANE (TYPHOON)",
+  "FLOOD",
+  "STORM SURGE/TIDE",
+  "ASTRONOMICAL LOW TIDE",
+  "COLD/WIND CHILL"
 )
 
-assign_event_cat(event_types, event_cats)
-
+assign_event_cat_sample(event_types, event_cats)
 
 # AFTER clean up
 # some name has unnecessary text afterwards
 for ( e in eventcat) {
-  sam1[startsWith(sam1$EVTYPE , e), "event_cat"] <- e
+  sam[startsWith(sam$EVTYPE , e), "event_cat"] <- e
 }
 
 # check 
-sam1_sum<-sam1 %>% 
+sam_sum<-sam %>% 
   filter(is.na(event_cat))  %>% 
   group_by(EVTYPE) %>% 
   summarize( 
